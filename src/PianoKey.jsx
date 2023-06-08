b5398442e8e69da90f728ab6874932573ccc5a1a
Adrian Shosholli <as227cw@student.lnu.se>
2023-06-08 03:07:49 +0200

    Make mouse event handling clearer and fix styling when leaving active key

--- src/PianoKey.jsx
+++ src/PianoKey.jsx
@@ -1,18 +1,41 @@
+import { useState } from 'react'
 import './Piano.css'
+import * as Tone from 'tone'
 
 function PianoKey({ note, onPianoKeyPress, onPianoKeyRelease, color }) {
+  const [pressed, setPressed] = useState(false)
+
+  function handleMouseDown(e) {
+    setPressed(true)
+    onPianoKeyPress(note)
+  }
+
+  function handleMouseUp(e) {
+    setPressed(false)
+    onPianoKeyRelease(note)
+  }
+
+  function handleMouseLeave(e) {
+    setPressed(false)
+    onPianoKeyRelease(note)
+  }
+
+  function handleMouseEnter(e) {
+    if (e.buttons === 1) {
+      setPressed(true)
+      onPianoKeyPress(note) 
+    }
+  }
+
   return (
     <>
       <button
-        className={color}
-        onMouseDown={() => onPianoKeyPress(note)}
-        onMouseUp={() => onPianoKeyRelease(note)}
-        onMouseLeave={() => onPianoKeyRelease(note)}
-        onMouseEnter={(e) => {
-          if (e.buttons === 1) { onPianoKeyPress(note) }
-        }
-        }
-      >{note}
+        className={`key ${color} ${pressed ? 'pressed' : ''}`}
+        onMouseDown={(e) => handleMouseDown(e)}
+        onMouseUp={(e) => handleMouseUp(e)}
+        onMouseLeave={(e) => handleMouseLeave(e)}
+        onMouseEnter={(e) => handleMouseEnter(e)}
+      >{Tone.Midi(note).toNote()}
       </button>
     </>
   )
